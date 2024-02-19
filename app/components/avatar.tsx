68a49d048a1a0c044aa8652b44b2083dd66e31c1
Marco TB <alvinmarco8@gmail.com>
2024-02-19 22:54:36 +0700

    update

---
+++ app/components/avatar.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+const Avatar = () => {
+    return(
+        <image 
+        className="rounded-full" 
+        height="30" 
+        width="30" 
+        src="/images/placeholder.jpg" />
+    );
+}
+
+export default Avatar
--- app/components/navbar/usermenu.tsx
+++ app/components/navbar/usermenu.tsx
@@ -1,5 +1,6 @@
-'use Client';
+'use client';
 import { AiOutlineMenu } from "react-icons/ai";
+import Avatar from '../avatar';
 
 const UserMenu = () => {
     return (
@@ -7,11 +8,14 @@ const UserMenu = () => {
         <div className="flex flex-row items-center gap-3">
             <div onClick={() => {}}
                 className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer">
-            Airbnb your home
+            Airbnb your home    
             </div>
             <div onClick={() => {}}
             className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition">
                 < AiOutlineMenu />
+            </div>
+            <div className="hidden md:block">
+                <Avatar />
             </div>
         </div>
     </div>
