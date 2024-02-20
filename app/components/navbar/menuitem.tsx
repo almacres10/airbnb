2d84b2618bdb7e29b85ec1b9f00add80d53b6455
ALVIN MARCO CRISTY TAMPUBOLON <817932702@intranet.pajak.go.id>
2024-02-20 15:02:53 +0700

    updat

--- app/components/navbar/menuitem.tsx
+++ app/components/navbar/menuitem.tsx
@@ -5,10 +5,14 @@ interface MenuItemProps{
     label: string;
 }
 
-const MenuItem = () => {
-    return(
-        <div></div>
+const MenuItem: React.FC<MenuItemProps>= ({
+    onClick,
+    label
+}) => {
+    return (
+        <div onClick={onClick} className="px-4 py-3 hover:bg-neutral-100 transition font-semibold">
+            {label}
+        </div>
     );
 }
-
 export default MenuItem;
