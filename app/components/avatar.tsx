2e0aebf448501b13997c402b1be2bb859e676fac
Marco TB <alvinmarco8@gmail.com>
2024-03-08 21:41:46 +0700

    update

--- app/components/avatar.tsx
+++ app/components/avatar.tsx
@@ -2,13 +2,19 @@
 
 import Image from 'next/image';
 
-const Avatar = () => {
-    return(
+interface AvatarProps{
+    src: string | null | undefined;
+};
+
+const Avatar: React.FC<AvatarProps> = ({
+    src }) => {
+    return (
         <Image 
         className="rounded-full" 
         height="30" 
         width="30"
-        src="/images/placeholder.jpg" 
+        alt="Avatar"
+        src={src || "/images/placeholder.jpg"} 
         />
     );
 }
